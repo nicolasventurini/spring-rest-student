b78bb4576c8d38af78c3fa0fded7ac50eef67ce7
nicolasventurini <nicolas.venturini@marconirovereto.it>
2024-05-07 08:28:22 +0200

    Creazione

---
+++ src/main/java/it/marconi/springreststudent/SpringRestStudentApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.springreststudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestStudentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/marconi/springreststudent/SpringRestStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package it.marconi.springreststudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
